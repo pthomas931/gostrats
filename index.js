1e1d5ea7c8c1fb481d7c20b24b9a8baca00ee2cb
Phillip Thomas <pthomas@excivity.com>
2020-02-14 11:41:29 -0500

    First crack at CSGO Strat Discord Bot

---
+++ index.js
@@ -0,0 +1,58 @@
+// Import the discord.js module
+const Discord = require("discord.js");
+
+const STRATS = require("./strats.json");
+
+// Create an instance of a Discord client
+const client = new Discord.Client();
+
+/**
+ * The ready event is vital, it means that only _after_ this will your bot start reacting to information
+ * received from Discord
+ */
+client.on("ready", () => {
+  console.log("I am ready!");
+});
+
+// Create an event listener for messages
+client.on("message", message => {
+  if (message.content.includes("/gostrats")) {
+    try {
+      const [command, map, playerCountStr] = message.content.split(" ");
+      const playerCount = Number.parseInt(playerCountStr) - 1;
+
+      message.channel.send(`Fetching strat for ${map} - ${playerCount}...`);
+
+      if (STRATS[map]) {
+        const theseStrats = STRATS[map][playerCount];
+        console.log("theseStrats: ", theseStrats);
+        if (
+          theseStrats &&
+          Array.isArray(theseStrats) &&
+          theseStrats.length > 0
+        ) {
+          const { overview, players } = theseStrats[
+            Math.floor(Math.random() * theseStrats.length)
+          ];
+
+          message.channel.send(overview);
+          console.log(players, playerCount);
+          players.forEach((playerStrat, index) =>
+            message.channel.send(`--- ${index + 1}: ${playerStrat}`)
+          );
+        } else {
+          message.channel.send(
+            `No strats for '${playerCount}' players on map '${map}'`
+          );
+        }
+      } else {
+        message.channel.send(`Unable to find map '${map}'.`);
+      }
+    } catch (e) {
+      console.log("error: ", e);
+    }
+  }
+});
+
+// Log our bot in using the token from https://discordapp.com/developers/applications/me
+client.login("TOKEN");
